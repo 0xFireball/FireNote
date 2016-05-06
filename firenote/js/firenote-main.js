ef63e5c3c3bca1df0f8c727625329279b2bbce61
0xFireball <0xFireball@outlook.com>
2016-05-05 23:23:12 -0700

    add dynamic notes

--- firenote/js/firenote-main.js
+++ firenote/js/firenote-main.js
@@ -5,18 +5,25 @@ $("document").ready(function () {
     }
   );
   $('.button-collapse').sideNav('hide');
+  $("#note-list-nav").append('<li><a href="javascript:void(0)" class="red lighten-3">New Note</a></li>');
 });
 
-$("#titlebar").click(function() {
+$("#titlebar").click(function () {
   $('.button-collapse').sideNav('show');
 });
 
 function initTinyMce(elementSelector) {
   tinymce.init({
     selector: elementSelector,
-    plugins: 'code',
-    menubar: false
+    plugins: 'code autoresize',
+    menubar: false,
   });
 }
+
+$("#delete-note-btn").click(function () {
+  if (confirm('Are you sure you want to delete this note? It cannot be recovered.')) {
+    //Delete
+  }
+});
 
 initTinyMce("#editarea");
