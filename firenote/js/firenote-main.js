c0ebed3675fbaa2991873681a0e1ff9829923e33
0xFireball <0xFireball@outlook.com>
2016-05-06 09:06:00 -0700

    add typescript and shortcuts

--- firenote/js/firenote-main.js
+++ firenote/js/firenote-main.js
@@ -1,29 +1,28 @@
-$("document").ready(function () {
-  $('.button-collapse').sideNav(
-    {
-      closeOnClick: true
-    }
-  );
-  $('.button-collapse').sideNav('hide');
-  $("#note-list-nav").append('<li><a href="javascript:void(0)" class="red lighten-3">New Note</a></li>');
+$("document").ready(function() {
+    $('.button-collapse').sideNav(
+        {
+            closeOnClick: true
+        }
+    );
+    $('.button-collapse').sideNav('hide');
+    $("#note-list-nav").append('<li><a href="javascript:void(0)" class="red lighten-3">New Note</a></li>');
 });
 
-$("#titlebar").click(function () {
-  $('.button-collapse').sideNav('show');
+$("#titlebar").click(function() {
+    $('.button-collapse').sideNav('show');
 });
 
 function initTinyMce(elementSelector) {
-  tinymce.init({
-    selector: elementSelector,
-    plugins: 'code autoresize',
-    menubar: false,
-  });
+    tinymce.init({
+        selector: elementSelector,
+        plugins: 'code autoresize',
+        menubar: false,
+        skin: "light"
+    });
 }
 
-$("#delete-note-btn").click(function () {
-  if (confirm('Are you sure you want to delete this note? It cannot be recovered.')) {
-    //Delete
-  }
-});
-
 initTinyMce("#editarea");
+
+shortcut.add("Ctrl+Space", function() {
+    $('.button-collapse').sideNav('show');
+});
--- firenote/js/firenote.js
+++ firenote/js/firenote.js
@@ -1,2 +1,35 @@
 /// <reference path="jquery.d.ts" />
+/// <reference path="lib.d.ts" />
+class FireNote {
+    loadNotes() {
+    }
+}
+jQuery.fn.selectText = function () {
+    var doc = document;
+    var element = this[0];
+    console.log(this, element);
+    if (doc.body.createTextRange) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    }
+    else if (window.getSelection) {
+        var selection = window.getSelection();
+        var range = document.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+};
+$("#delete-note-btn").click(function () {
+    if (confirm('Are you sure you want to delete this note? It cannot be recovered.')) {
+    }
+});
+$("#rename-note-btn").click(function () {
+    var titleBar = $("#titlebar");
+    titleBar.prop("contenteditable", true);
+    titleBar.focus();
+    titleBar.selectText();
+    titleBar.keypress(function (e) { return e.which != 13; });
+});
 //# sourceMappingURL=firenote.js.map
