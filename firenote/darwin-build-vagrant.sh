b4202dd67663f6dcda4743dd35e0be50a993a6ef
0xFireball <0xFireball@outlook.com>
2016-05-07 13:06:24 -0700

    use home for tmp

--- firenote/darwin-build-vagrant.sh
+++ firenote/darwin-build-vagrant.sh
@@ -1,16 +1,17 @@
 APPNAME="FireNote"
+BUILD_DIR="$HOME/tmp/"
 
 echo "Starting OS X - 64-bit build of $APPNAME"
 rm -Rf ./build/darwin64
 mkdir -pv ./build/darwin64/
-electron-packager . $APPNAME --platform=darwin --arch=all --out=/tmp/$APPNAME/build/
-rm /tmp/$APPNAME/build/$APPNAME-darwin-x64/$APPNAME.app/Contents/Resources/electron.icns
-cp ./img/$APPNAME.icns /tmp/$APPNAME/build/$APPNAME-darwin-x64/$APPNAME.app/Contents/Resources/electron.icns
+electron-packager . $APPNAME --platform=darwin --arch=all --out=BUILD_DIR$APPNAME/build/
+rm BUILD_DIR$APPNAME/build/$APPNAME-darwin-x64/$APPNAME.app/Contents/Resources/electron.icns
+cp ./img/$APPNAME.icns BUILD_DIR$APPNAME/build/$APPNAME-darwin-x64/$APPNAME.app/Contents/Resources/electron.icns
 
 CURRENTPATH=$(pwd)
-cd /tmp/$APPNAME/build/$APPNAME-darwin-x64/
+cd BUILD_DIR$APPNAME/build/$APPNAME-darwin-x64/
 
 zip -r -9 --symlinks $CURRENTPATH/build/darwin64/$APPNAME.app.zip $APPNAME.app
 cd $CURRENTPATH
-rm -Rf /tmp/$APPNAME/build/
+rm -Rf BUILD_DIR$APPNAME/build/
 echo "The output can be found in /build/darwin64 if the build succeeded, and the file should be called $APPNAME.app.zip"
