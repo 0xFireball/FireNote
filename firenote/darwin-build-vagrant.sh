6b86aeaa9d45ac1dab8231a82289decc340426f6
0xFireball <0xFireball@outlook.com>
2016-05-07 16:36:57 -0700

    list outp

--- firenote/darwin-build-vagrant.sh
+++ firenote/darwin-build-vagrant.sh
@@ -23,4 +23,5 @@ cd $CURRENTPATH
 echo "Cleaning Up..."
 rm -Rf $BUILD_DIR/$APPNAME/build/
 echo "The output can be found in /build/darwin64 if the build succeeded, and the file should be called $APPNAME.app.zip"
-echo "Working Directory: $(pwd)"
+echo "Output Stuff:"
+ls $OUTPATH
