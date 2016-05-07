fff3fe0063e4818c9b348bb273c45464e62c2e68
0xFireball <0xFireball@outlook.com>
2016-05-07 16:04:03 -0700

    clean p

--- firenote/darwin-build-vagrant.sh
+++ firenote/darwin-build-vagrant.sh
@@ -1,5 +1,5 @@
 APPNAME="FireNote"
-BUILD_DIR="$HOME/tmp/"
+BUILD_DIR="$HOME/tmp"
 OUTPATH="build/darwin64/"
 
 echo "Starting OS X - 64-bit build of $APPNAME"
@@ -7,13 +7,13 @@ rm -Rf ./build/darwin64
 mkdir -pv ./$OUTPATH
 
 echo "Running Electron Packager..."
-electron-packager . $APPNAME --platform=darwin --arch=all --out=$BUILD_DIR$APPNAME/build/
+electron-packager . $APPNAME --platform=darwin --arch=all --out=$BUILD_DIR/$APPNAME/build/
 echo "Replacing Icon..."
-rm $BUILD_DIR$APPNAME/build/$APPNAME-darwin-x64/$APPNAME.app/Contents/Resources/electron.icns
-cp ./img/$APPNAME.icns $BUILD_DIR$APPNAME/build/$APPNAME-darwin-x64/$APPNAME.app/Contents/Resources/electron.icns
+rm $BUILD_DIR/$APPNAME/build/$APPNAME-darwin-x64/$APPNAME.app/Contents/Resources/electron.icns
+cp ./img/$APPNAME.icns $BUILD_DIR/$APPNAME/build/$APPNAME-darwin-x64/$APPNAME.app/Contents/Resources/electron.icns
 
 CURRENTPATH=$(pwd)
-cd $BUILD_DIR$APPNAME/build/$APPNAME-darwin-x64/
+cd $BUILD_DIR/$APPNAME/build/$APPNAME-darwin-x64/
 
 ARCPATH="$CURRENTPATH/$OUTPATH$APPNAME.app.zip"
 echo "Compressing to $ARCPATH"
@@ -21,6 +21,6 @@ zip -r -9 --symlinks $ARCPATH $APPNAME.app > /dev/null
 cd $CURRENTPATH
 
 echo "Cleaning Up..."
-rm -Rf $BUILD_DIR$APPNAME/build/
+rm -Rf $BUILD_DIR/$APPNAME/build/
 echo "The output can be found in /build/darwin64 if the build succeeded, and the file should be called $APPNAME.app.zip"
 echo "Working Directory: $(pwd)"
