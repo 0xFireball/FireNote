4f656b09702d6a8cbe92549a026e159c778c8e25
0xFireball <0xFireball@outlook.com>
2016-05-06 23:59:08 -0700

    copy icons

--- firenote/darwin-build-vagrant.sh
+++ firenote/darwin-build-vagrant.sh
@@ -2,6 +2,8 @@ echo "Starting OS X - 64-bit build of FireNote"
 rm -Rf ./build/darwin64
 mkdir -pv ./build/darwin64/
 electron-packager . FireNote --platform=darwin --arch=all --out=/tmp/firenote/build/
+rm /tmp/firenote/build/FireNote-darwin-x64/FireNote.app/Contents/Resources/electron.icns
+cp ./img/firenote.icns /tmp/firenote/build/FireNote-darwin-x64/FireNote.app/Contents/Resources/electron.icns
 CURRENTPATH=$(pwd)
 cd /tmp/firenote/build/FireNote-darwin-x64/
 zip -r -9 --symlinks $CURRENTPATH/build/darwin64/FireNote.app.zip FireNote.app
