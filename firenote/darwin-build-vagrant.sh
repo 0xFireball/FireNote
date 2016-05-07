c53c1e185dabf8261f1f1fe17a760d962b8ed889
0xFireball <0xFireball@outlook.com>
2016-05-07 13:17:03 -0700

    more  verbose script

--- firenote/darwin-build-vagrant.sh
+++ firenote/darwin-build-vagrant.sh
@@ -4,7 +4,10 @@ BUILD_DIR="$HOME/tmp/"
 echo "Starting OS X - 64-bit build of $APPNAME"
 rm -Rf ./build/darwin64
 mkdir -pv ./build/darwin64/
+
+echo "Running Electron Packager..."
 electron-packager . $APPNAME --platform=darwin --arch=all --out=BUILD_DIR$APPNAME/build/
+echo "Replacing Icon..."
 rm BUILD_DIR$APPNAME/build/$APPNAME-darwin-x64/$APPNAME.app/Contents/Resources/electron.icns
 cp ./img/$APPNAME.icns BUILD_DIR$APPNAME/build/$APPNAME-darwin-x64/$APPNAME.app/Contents/Resources/electron.icns
 
@@ -13,5 +16,7 @@ cd BUILD_DIR$APPNAME/build/$APPNAME-darwin-x64/
 
 zip -r -9 --symlinks $CURRENTPATH/build/darwin64/$APPNAME.app.zip $APPNAME.app
 cd $CURRENTPATH
+
+echo "Cleaning Up..."
 rm -Rf BUILD_DIR$APPNAME/build/
 echo "The output can be found in /build/darwin64 if the build succeeded, and the file should be called $APPNAME.app.zip"
